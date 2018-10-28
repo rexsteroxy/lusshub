108fdf08d9dc608cac4a1cbce736e815c72be607
emeto winner <emetowinner@gmail.com>
2018-10-28 08:08:11 -0800

    light changes to the style shit

--- contact.php
+++ contact.php
@@ -50,7 +50,9 @@
 			  <div class="container main-menu">
 				  <div class="row align-items-center justify-content-between d-flex">
 					<div id="logo">
-					  <a href="index.php"><img src="img/logo.png" alt="" title="" /></a>
+					  <a href="index.php"><h4>
+								Connect || <b>L</b>ss<b>H</b>ub
+							</h4></a>
 					</div>
 					<nav id="nav-menu-container">
 					  <ul class="nav-menu">
@@ -62,8 +64,6 @@
 						  </ul>
 						</li>	
 						<li><a href="sales.php">QS</a></li>
-						<li><a href="about.php">About</a></li>
-						<li><a href="contact.php">Contact</a></li>
 						<li class="menu-has-children">Account
 						  <ul>
 							<li><a href="login.php"><i class="fa fa-sign-in"></i>Login</a></li>
@@ -172,7 +172,7 @@
 							<div class="work-icon-box">
 								<span class="lnr lnr-train"></span>
 							</div>
-							<h4 class="caption">Signup</h4>
+							<p class="caption">Signup</p>
 						</div>
 						<div class="work-arrow">
 							<img src="img/arrow.png" alt="">
@@ -181,7 +181,7 @@
 							<div class="work-icon-box">
 								<span class="lnr lnr-layers"></span>
 							</div>
-							<h4 class="caption">Join a division</h4>
+							<p class="caption">Join a division</p>
 						</div>
 						<div class="work-arrow">
 							<img src="img/arrow.png" alt="">
@@ -190,7 +190,7 @@
 							<div class="work-icon-box">
 								<span class="lnr lnr-database"></span>
 							</div>
-							<h4 class="caption">Update PS board</h4>
+							<p class="caption">Update PS board</p>
 						</div>
 						<div class="work-arrow">
 							<img src="img/arrow.png" alt="">
@@ -199,7 +199,7 @@
 							<div class="work-icon-box">
 								<span class="lnr lnr-smile"></span>
 							</div>
-							<h4 class="caption">You're connected</h4>
+							<p class="caption">You're connected</p>
 						</div>											
 					<div class="row">
 						<div class="col"></div>
@@ -282,6 +282,7 @@
 											<li><a href="#">Home</a></li>
 											<li><a href="#">Feature</a></li>
 											<li><a href="#">Services</a></li>
+											<li><a href="about.php">About</a></li>
 										</ul>
 									</div>
 									<div class="col">
@@ -289,6 +290,7 @@
 											<li><a href="#">Team</a></li>
 											<li><a href="#">Pricing</a></li>
 											<li><a href="#">Contact</a></li>
+											<li><a href="contact.php">Contact</a></li>
 										</ul>
 									</div>										
 								</div>							
@@ -318,7 +320,7 @@
 
 					<div class="row footer-bottom d-flex justify-content-between align-items-center">
 						<p class="col-lg-8 col-sm-12 footer-text m-0"><!-- Link back to lusshub can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with by <a href="#" target="_blank">Winner</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with by <a href="#" target="_blank">LussHub</a>
 <!-- Link back to lusshub can't be removed. Template is licensed under CC BY 3.0. -->
 </p>
 						<div class="col-lg-4 col-sm-12 footer-social">
