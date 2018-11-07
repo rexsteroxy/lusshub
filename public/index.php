e49b672ca10c3a5430201700523481f4b9a431f0
emeto winner <emetowinner@gmail.com>
2018-11-07 01:04:41 -0800

    update

--- public/index.php
+++ public/index.php
@@ -72,29 +72,7 @@
 						</p>
 					</div>
 				</div>
-<<<<<<< HEAD
-			</div>
-		</div>
 	</section>
-	<!-- End qs Area -->
-
-
-	<!-- Start brands Area -->
-	<section class="brands-area pb-120">
-		<div class="container no-padding">
-			<div class="brand-wrap">
-				<div class="row align-items-center active-brand-carusel justify-content-start no-gutters">
-					<div class="col single-brand">
-						<a href="#">
-							<img class="mx-auto" src="img/l1.png" alt="">
-						</a>
-					</div>
-					<div class="col single-brand">
-						<a href="#">
-							<img class="mx-auto" src="img/l2.png" alt="">
-						</a>
-=======
-			</section>
 			<!-- End banner Area -->	
 						
 			
@@ -156,8 +134,6 @@
 								</p>
 							</div>
 						</div>
->>>>>>> 2e5f0d1db522fb3f029649ad182296bffbd28043
-					</div>
 					<div class="col single-brand">
 						<a href="#">
 							<img class="mx-auto" src="img/l3.png" alt="">
